3d0f39bdec5069858467d928bf0037af14114cad
diac <nikolai.gladkikh@gmail.com>
2022-09-27 17:34:35 +0300

    1. Отчеты. [#850]

---
+++ src/main/java/ru/job4j/srp/employee/BetterReportEngine.java
@@ -0,0 +1,52 @@
+package ru.job4j.srp.employee;
+
+import java.text.DecimalFormat;
+import java.util.function.Predicate;
+
+public class BetterReportEngine implements Report {
+
+    public static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("0.00");
+
+    private Store store;
+
+    public BetterReportEngine(Store store) {
+        this.store = store;
+    }
+
+    @Override
+    public String generate(Predicate<Employee> filter) {
+        StringBuilder text = new StringBuilder();
+        text.append("""
+                <html>
+                <head></head>
+                <body>
+                    <table>""");
+        text.append("""
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Salary</th>
+                            </tr>
+                        </thead>
+                        <tbody>""")
+                .append(System.lineSeparator());
+        for (Employee employee
+                : store.findBy(filter)
+                    .stream()
+                    .sorted((left, right) -> left.getSalary() > right.getSalary() ? -1 : 1)
+                    .toList()
+        ) {
+            text.append("<tr>");
+            text.append("<td>").append(employee.getName()).append("</td>");
+            text.append("<td>").append("$" + DECIMAL_FORMAT.format(employee.getSalary())).append("</td>");
+            text.append("</tr>");
+        }
+        text.append("""
+                        </tbody>
+                    </table>
+                </body>
+                </html>
+                """);
+        return text.toString();
+    }
+}
--- src/test/java/ru/job4j/srp/employee/ReportEngineTest.java
+++ src/test/java/ru/job4j/srp/employee/ReportEngineTest.java
@@ -6,6 +6,8 @@ import java.util.Calendar;
 
 import static ru.job4j.srp.employee.ReportEngine.DATE_FORMAT;
 
+import static ru.job4j.srp.employee.BetterReportEngine.DECIMAL_FORMAT;
+
 import static org.assertj.core.api.Assertions.*;
 
 class ReportEngineTest {
@@ -25,6 +27,47 @@ class ReportEngineTest {
                 .append(DATE_FORMAT.format(worker.getFired().getTime())).append(";")
                 .append(worker.getSalary()).append(";")
                 .append(System.lineSeparator());
+        assertThat(engine.generate(em -> true)).isEqualTo(expect.toString());
+    }
+
+    @Test
+    public void whenNewGenerated() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee ivan = new Employee("Ivan", now, now, 100);
+        Employee stepan = new Employee("Stepan", now, now, 200);
+        store.add(ivan);
+        store.add(stepan);
+        Report engine = new BetterReportEngine(store);
+        StringBuilder expect = new StringBuilder();
+        expect.append("""
+                <html>
+                <head></head>
+                <body>
+                    <table>""");
+        expect.append("""
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Salary</th>
+                            </tr>
+                        </thead>
+                        <tbody>""")
+                .append(System.lineSeparator());
+        expect.append("<tr>");
+        expect.append("<td>").append(stepan.getName()).append("</td>");
+        expect.append("<td>").append("$" + DECIMAL_FORMAT.format(stepan.getSalary())).append("</td>");
+        expect.append("</tr>");
+        expect.append("<tr>");
+        expect.append("<td>").append(ivan.getName()).append("</td>");
+        expect.append("<td>").append("$" + DECIMAL_FORMAT.format(ivan.getSalary())).append("</td>");
+        expect.append("</tr>");
+        expect.append("""
+                        </tbody>
+                    </table>
+                </body>
+                </html>
+                """);
         assertThat(engine.generate(em -> true)).isEqualTo(expect.toString());
     }
 }
