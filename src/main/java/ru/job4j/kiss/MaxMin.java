a9d7a74601c967c840abc5cfa30b52a01e57dedb
diac <nikolai.gladkikh@gmail.com>
2022-09-27 14:24:31 +0300

    Упростить код через BiPredicate: 1. Принципы Kiss, Dry и Yagni [#238813 #346654]

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -1,33 +1,30 @@
 package ru.job4j.kiss;
 
 import java.util.*;
+import java.util.function.BiPredicate;
 
 public class MaxMin {
 
-    private <T> List<T> maxMin(List<T> value, Comparator<T> comparator) {
-        List<T> result = Arrays.asList(null, null);
-        if (value.size() >= 2) {
-            result.set(0, value.get(0));
-            result.set(1, value.get(value.size() - 1));
-            for (var item : value) {
-                if (Objects.compare(result.get(0), item, comparator) > 0) {
-                    result.set(0, item);
-                }
-                if (Objects.compare(result.get(1), item, comparator) <= 0) {
-                    result.set(1, item);
+    private <T> T maxMin(List<T> value, BiPredicate<T, T> predicate) {
+        T result = null;
+        if (value.size() > 0) {
+            result = value.get(0);
+            for (var i = 1; i < value.size(); i++) {
+                if (predicate.test(result, value.get(i))) {
+                    result = value.get(i);
                 }
             }
-        } else if (value.size() == 1) {
-            result = Arrays.asList(value.get(0), value.get(0));
         }
         return result;
     }
 
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        return maxMin(value, comparator).get(1);
+        BiPredicate<T, T> predicate = (left, right) -> comparator.compare(left, right) <= 0;
+        return maxMin(value, predicate);
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        return maxMin(value, comparator).get(0);
+        BiPredicate<T, T> predicate = (left, right) -> comparator.compare(left, right) > 0;
+        return maxMin(value, predicate);
     }
 }
