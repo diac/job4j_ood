ac8cfc02d53fdd81652af51af8f3a638fb502f0e
diac <nikolai.gladkikh@gmail.com>
2022-09-29 11:37:35 +0300

    Вынести пороговые значения в отдельный класс для констант: 1. Хранилище продуктов [#852 #347096]

---
+++ src/main/java/ru/job4j/food/FoodExpiryThresholds.java
@@ -0,0 +1,8 @@
+package ru.job4j.food;
+
+public class FoodExpiryThresholds {
+
+    public static final byte SPOILED = 100;
+    public static final byte DISCOUNT = 75;
+    public static final byte FRESH = 25;
+}
