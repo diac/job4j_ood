31cd8e4cb827d98b43ade6161631432c320beee4
diac <nikolai.gladkikh@gmail.com>
2022-09-30 22:00:11 +0300

    Сделать метод Shop.applyDiscount() приватным: 1. Хранилище продуктов [#852 #347096]

--- src/main/java/ru/job4j/food/Shop.java
+++ src/main/java/ru/job4j/food/Shop.java
@@ -28,7 +28,7 @@ public class Shop implements Store {
         return new ArrayList<>(foods);
     }
 
-    void applyDiscount(Food food) {
+    private void applyDiscount(Food food) {
         if (getFoodExpiryRate(food) >= FoodExpiryThresholds.DISCOUNT) {
             food.setPrice(food.getPrice() - food.getPrice() * food.getDiscount() / 100);
         }
