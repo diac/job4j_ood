f92fc71f6fd86094bba1ae722b6ea7274a5cfca8
diac <nikolai.gladkikh@gmail.com>
2022-09-30 11:25:44 +0300

    Добавить в парковку необходимые поля и конструктор: 2. Парковка машин [#853 #347100]

--- src/main/java/ru/job4j/parking/SimpleParking.java
+++ src/main/java/ru/job4j/parking/SimpleParking.java
@@ -1,8 +1,20 @@
 package ru.job4j.parking;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class SimpleParking implements Parking {
+
+    private Set<Car> passengerCars = new HashSet<>();
+    private Set<Car> trucks = new HashSet<>();
+    private final int availablePassengerCarPlaces;
+    private final int availableTruckPlaces;
+
+    public SimpleParking(int availablePassengerCarPlaces, int availableTruckPlaces) {
+        this.availablePassengerCarPlaces = availablePassengerCarPlaces;
+        this.availableTruckPlaces = availableTruckPlaces;
+    }
 
     @Override
     public boolean park(Car car) {
